f8fdda73b9f9349981b0310d7197399fdf7e4fc0
Christian Galsterer <christian.galsterer@gmx.de>
2024-12-20 08:38:56 +0100

    docs(dbt): added models for all marts

--- dbt.sh
+++ dbt.sh
@@ -5,4 +5,12 @@ source dbt_packages/codegen/bash_scripts/base_model_creation.sh "novelan" ["heat
 dbt run-operation codegen.create_base_models --args '{source_name: novelan, tables: ["heatpump"]}'
 dbt run-operation generate_base_model --args '{"source_name": "novelan", "table_name": "heatpump"}'
 
-dbt run-operation generate_model_yaml --args '{"model_names": ["stg_novelan__heatpumps"]}' > models/staging/novelan/_novelan__sources.yml
+dbt run-operation generate_model_yaml --args '{"model_names": ["stg_novelan__heatpumps"]}'
+dbt run-operation generate_model_yaml --args '{"model_names": ["heatpump_compressor_usage"]}'
+dbt run-operation generate_model_yaml --args '{"model_names": ["heatpump_heat_quantities_daily"]}'
+dbt run-operation generate_model_yaml --args '{"model_names": ["heatpump_heat_quantities_monthly"]}'
+dbt run-operation generate_model_yaml --args '{"model_names": ["heatpump_heat_quantities_yearly"]}'
+dbt run-operation generate_model_yaml --args '{"model_names": ["heatpump_heat_quantities"]}'
+dbt run-operation generate_model_yaml --args '{"model_names": ["heatpump_temperatures"]}'
+
+
