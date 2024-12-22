cdf9e560c42b738111383adc3feb5a5301ded4db
Christian Galsterer <christian.galsterer@gmx.de>
2024-12-22 09:48:12 +0100

    test: use larger data set

--- scripts/duckdb_load.sql
+++ scripts/duckdb_load.sql
@@ -1,3 +1,3 @@
 CREATE SCHEMA IF NOT EXISTS raw;
 DROP TABLE IF EXISTS raw.novelan_heatpump;
-CREATE TABLE IF NOT EXISTS raw.novelan_heatpump AS SELECT * FROM read_csv('ingest/novelan/heatpump/v2/*.csv', decimal_separator = ',');
+CREATE TABLE IF NOT EXISTS raw.novelan_heatpump AS SELECT * FROM read_csv('ingest/novelan/heatpump/v2/*.csv', decimal_separator = ',', types={'AnalogOut_2': 'VARCHAR', 'TBW_Soll': 'VARCHAR'});
