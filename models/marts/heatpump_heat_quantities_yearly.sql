89956bf6a1075e7ac63e3f02928144e8d4a17884
Christian Galsterer <christian.galsterer@gmx.de>
2025-01-23 08:03:24 +0100

    fix(dbt): calculate heat quantity yearly aggregation from  single values as monthly aggregations may not contain all days.
    This provides a more accurate result on a yearly aggregation although the sum of the monthly values may not add up to the yearly values anymore due to missing data

--- models/marts/heatpump_heat_quantities_yearly.sql
+++ models/marts/heatpump_heat_quantities_yearly.sql
@@ -2,20 +2,20 @@ with
 
 source as (
 
-    select * from {{ ref('heatpump_heat_quantities_monthly') }}
+    select * from {{ ref('heatpump_heat_quantities') }}
 
 ),
 
 yearly_values as
     (
     select 
-        date_trunc('year', month) as year,
-        sum(heat_quantity_heating) as heat_quantity_heating,
-        sum(heat_quantity_water) as heat_quantity_water
+        date_trunc('year', created_date) as year,
+        max(heat_quantity_heating) - min(heat_quantity_heating) as heat_quantity_heating,
+        max(heat_quantity_water) - min(heat_quantity_water) as heat_quantity_water
     from source
-    group by date_trunc('year', month)
+    where heat_quantity_heating > 0 and heat_quantity_water > 0
+    group by date_trunc('year', created_date)
     order by year desc
 )
 
 select * from yearly_values
-
